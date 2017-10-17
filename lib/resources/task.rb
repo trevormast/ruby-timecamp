9f9ac4ea507109723af9b593fdaac493c9bedfe3
Trevor Mast <trevormast@gmail.com>
2017-10-16 21:21:53 -0700

    finishes tasks API

--- lib/resources/task.rb
+++ lib/resources/task.rb
@@ -9,8 +9,31 @@ module TimeCamp
       response.each { |k,v| instance_variable_set("@#{k.underscore}", v) }
     end
 
+    # optional :task_id
     def self.get(opts={})
       response = TimeCamp::Request.get(resource_name, opts)
+      return TaskCollection.parse(response)
+    end
+
+    # required :name
+    # optional :tags, :parent_id, :external_task_id, :external_parent_id,
+    #          :budgeted, :note, :archived, :billable, :budget_unit,
+    #          :user_ids (comma separated), :role
+    def self.create(opts={})
+      data_keys = [:name, :tags, :parent_id, :external_task_id, :external_parent_id, :budgeted, :note, :archived, :billable, :budget_unit, :user_ids, :role]
+      data, opts = opts.partition{ |k, v| data_keys.include?(k) }.map(&:to_h)
+      response = TimeCamp::Request.post(resource_name, opts, data)
+      return TaskCollection.parse(response)
+    end
+
+    # required :task_id
+    # optional :name, :tags, :parent_id, :external_task_id,
+    #          :external_parent_id, :budgeted, :note, :archived,
+    #          :billable, :budget_unit, :user_ids (comma separated), :role
+    def self.update(opts={})
+      data_keys = [:task_id, :name, :tags, :parent_id, :external_task_id, :external_parent_id, :budgeted, :note, :archived, :billable, :budget_unit, :user_ids, :role]
+      data, opts = opts.partition{ |k, v| data_keys.include?(k) }.map(&:to_h)
+      response = TimeCamp::Request.put(resource_name, opts, data)
       return TaskCollection.parse(response)
     end
   end
