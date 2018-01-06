aa5f4909977b90311aa66806ae37378d03f99a24
Trevor Mast <trevormast@gmail.com>
2018-01-05 17:26:17 -0800

    adds timer spec

---
+++ spec/timer_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe TimeCamp::Timer do
+  describe '.start' do
+    it 'starts a timer' do
+      VCR.use_cassette('timer/start') do
+        timer = TimeCamp::Timer.start
+        expect(timer.class).to eq(TimeCamp::Timer)
+        expect(timer.instance_variables).to eq([:@new_timer_id, :@entry_id])
+      end
+    end
+  end
+
+  describe '.get' do
+    it 'gets timers' do
+      VCR.use_cassette('timer/get') do
+        timers = TimeCamp::Timer.get
+        expect(timers.class).to eq(TimeCamp::Timer)
+        expect(timers.instance_variables).to eq([:@entry_id, :@is_timer_running, :@elapsed, :@timer_id, :@start_time])
+      end
+    end
+  end
+
+  describe '.stop' do
+    it 'stops a timer' do
+      VCR.use_cassette('timer/get') { @timer = TimeCamp::Timer.get }
+
+      VCR.use_cassette('timer/stop') do
+        timers = TimeCamp::Timer.stop(timer_id: @timer.timer_id, stopped_at: DateTime.now.strftime('%Y-%m-%d %H:%M:%S'))
+        expect(timers.class).to eq(TimeCamp::Timer)
+        expect(timers.instance_variables).to eq([:@entry_id, :@elapsed, :@entry_time])
+        expect(timers.elapsed).to eq(839)
+      end
+    end
+  end
+
+  # TODO: get without running timer, get with multiple running timers
+end
