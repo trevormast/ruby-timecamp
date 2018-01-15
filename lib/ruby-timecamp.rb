338ee136948a3721465ca69bac95214bc604f950
Trevor Mast <trevormast@gmail.com>
2018-01-14 20:49:02 -0800

    bundles from gemspec

--- lib/ruby-timecamp.rb
+++ lib/ruby-timecamp.rb
@@ -3,8 +3,3 @@ Dir["./lib/ruby-timecamp/*.rb"].each {|file| require file }
 Dir["./lib/ruby-timecamp/resources/*.rb"].each {|file| require file }
 require 'active_support/all'
 require 'rest-client'
-require 'pry'
-# dont require these for testing
-unless (ENV['TIMECAMP_ENV'] == 'test')
-  require 'httplog'
-end
