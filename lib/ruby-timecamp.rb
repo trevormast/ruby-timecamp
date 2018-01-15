9bd3631d88c86f35c835a93b467d762419e85af0
Trevor Mast <trevormast@gmail.com>
2018-01-14 20:41:45 -0800

    reorganizes directory tree

--- lib/ruby-timecamp.rb
+++ lib/ruby-timecamp.rb
@@ -1,6 +1,6 @@
 # require everything in lib
-Dir["./lib/*.rb"].each {|file| require file }
-Dir["./lib/resources/*.rb"].each {|file| require file }
+Dir["./lib/ruby-timecamp/*.rb"].each {|file| require file }
+Dir["./lib/ruby-timecamp/resources/*.rb"].each {|file| require file }
 require 'active_support/all'
 require 'rest-client'
 require 'pry'
