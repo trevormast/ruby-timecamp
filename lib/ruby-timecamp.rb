9f89435584f30a1f9d3fe2641b6941329f92c444
Trevor Mast <trevormast@gmail.com>
2018-04-14 14:41:13 -0700

    autoload all classes

--- lib/ruby-timecamp.rb
+++ lib/ruby-timecamp.rb
@@ -5,5 +5,16 @@ require 'active_support/all'
 require 'rest-client'
 
 module TimeCamp
-  autoload :Request,      'ruby-timecamp/request'
+  autoload :Request,              'ruby-timecamp/request'
+  autoload :Resource,             'ruby-timecamp/resource'
+  autoload :ResourceCollection,   'ruby-timecamp/resource_collection'
+  autoload :Response,             'ruby-timecamp/response'
+
+  autoload :Entry,                'ruby-timecamp/resources/entry'
+  autoload :EntryCollection,      'ruby-timecamp/resources/entry_collection'
+  autoload :Task,                 'ruby-timecamp/resources/task'
+  autoload :TaskCollection,       'ruby-timecamp/resources/task_collection'
+  autoload :Timer,                'ruby-timecamp/resources/timer'
+  autoload :User,                 'ruby-timecamp/resources/user'
+  autoload :UserCollection,       'ruby-timecamp/user_collection'
 end
